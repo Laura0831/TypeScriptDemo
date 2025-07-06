756d9e7e0710074b09a7c212d7c5c841fd23ab84
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-07-06 13:16:19 -0700

    practicing

--- Project1.ts
+++ Project1.ts
@@ -1,8 +1,18 @@
-
+//String variable declaration  
 let Lname: String;
 Lname = "laura"; 
 let newName = Lname.toUpperCase();
-
 console.log(Lname);
 console.log(newName);
+
+
+
+//Number variable declaration
+let age: number;
+age = 30.5
+console.log(age);
+
+
+
+
 
