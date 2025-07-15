7e00b2c8845d4c50e99e245a2c5f5b8df7600c90
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-07-15 16:38:46 -0700

    working on basics

---
+++ class.ts
@@ -0,0 +1,13 @@
+ class Employee{
+    id!: number;
+    name!: String;
+    address!: string;
+ }
+
+ let john = new Employee();
+
+ john.id = 1;
+ john.name = "John Monroe";
+ john.address = "2425 type st. Maine USA";
+
+    console.log(john.name);
