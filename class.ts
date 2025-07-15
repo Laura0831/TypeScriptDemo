c7da6fd87f5c50f2055a5a13c014608323b89ee0
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-07-15 16:40:27 -0700

    working on basics

--- class.ts
+++ class.ts
@@ -2,12 +2,18 @@
     id!: number;
     name!: String;
     address!: string;
+
+    constructor(id: number, name: string, address: string) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
  }
 
- let john = new Employee();
+ let john = new Employee(1, "John Monroe", "2425 type st. Maine USA");
 
- john.id = 1;
- john.name = "John Monroe";
- john.address = "2425 type st. Maine USA";
+//  john.id = 1;
+//  john.name = "John Monroe";
+//  john.address = "2425 type st. Maine USA";
 
     console.log(john.name);
